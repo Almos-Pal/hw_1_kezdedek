84c714d6827e6cbcc56cdc69f3e7d92cadf959c0
Pál Álmos Máté <pal.almos04@gmail.com>
2021-04-17 14:25:43 +0200

    first commit

---
+++ js/myjs.js
@@ -0,0 +1,19 @@
+
+function Myfunction() {
+  document.getElementById("eredmeny").innerHTML = "";
+  document.querySelector("#kek").innerHTML = "";
+  document.querySelector("#piros").innerHTML = "";
+  document.querySelector("#szurke").innerHTML = "";
+  document.querySelector("#kek2").innerHTML = "";
+  document.querySelector("#piros2").innerHTML = "";
+  document.querySelector("#szurke2").innerHTML = "";
+}
+function Myfunction2() {
+    document.getElementById("eredmeny").innerHTML = "069";
+    document.querySelector("#kek").innerHTML = "6";
+    document.querySelector("#piros").innerHTML = "9";
+    document.querySelector("#szurke").innerHTML = "0";
+    document.querySelector("#kek2").innerHTML = "6";
+    document.querySelector("#piros2").innerHTML = "9";
+    document.querySelector("#szurke2").innerHTML = "0";
+  }
